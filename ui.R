d6e14394ce0e1707afbadde3ff23820e808d0ed7
Greg Lambert <glambert2010@icloud.com>
2016-07-11 17:39:07 -0600

    Zika visualization

--- ui.R
+++ ui.R
@@ -8,11 +8,11 @@ shinyUI(fluidPage(
   sidebarPanel(
   
     selectizeInput("name",
-                   label = "Select US Teritory or State",
+                   label = "Select U.S. Teritory or State",
                    choices = unique(ideal$location),
                    multiple = T,
                    options = list(maxItems = 8, placeholder = 'Select a name'),
-                   selected = c("New_York","Florida")),
+                   selected = c("California", "New_York")),
     selectizeInput("type",
                    label = "Select Local or Travel Event",
                    choices = unique(ideal$data_field),
@@ -20,8 +20,8 @@ shinyUI(fluidPage(
                    options = list(maxItems = 1, placeholder = 'Select an event'),
                    selected = c("zika_reported_travel")),
     # Term plot
-    plotOutput("termPlot", height = 50),
-    helpText("Data: ")
+    plotOutput("termPlot", height = 30),
+    helpText("Data: CDC zika repo (https://github.com/cdcepi/zika)")
   ),
   # Show a plot of the generated distribution
   mainPanel(
