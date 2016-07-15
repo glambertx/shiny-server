2aab0d432f08a3d42c78bd271cc5f0302b9ffa73
Greg Lambert <glambert2010@icloud.com>
2016-07-14 23:56:15 -0600

    zoom continental U.S.

--- choropleth/server.r
+++ choropleth/server.r
@@ -7,7 +7,7 @@ library(ggplot2)
 library(RColorBrewer)
 pdf(NULL)
 df1 <- read.csv("zika_clean.csv")
-
+data(continental_us_states)
 
 #zika_clean <- read.csv("zika_clean.csv", header=T)
 names(df1) <- c("region","6","5","4","3","2")
@@ -30,7 +30,7 @@ df1 <- df1[c(1,6,5,4,3,2)]
       year = as.numeric(input$month)
       df1$value= df1[,input$month]
       #p <- state_choropleth(df1, num_colors = input$num_colors)
-      state_choropleth(df1, num_colors = 1) + fill_scale
+      state_choropleth(df1, num_colors = 1, zoom=continental_us_states) + fill_scale
       #print(p + fill_scale)
     })
   
--- choropleth/ui.r
+++ choropleth/ui.r
@@ -5,14 +5,12 @@ library(ggplot2)
 
 df1 <- read.csv("zika_clean.csv")
 
-#load("zika_clean.RData")
-#zika_clean <- read.csv("zika_clean.csv", header=T)
 names(df1) <- c("region","6","5","4","3","2")
 df1 <- df1[c(1,6,5,4,3,2)]
 
 shinyUI(fluidPage(
   
-  titlePanel("Zika Travel Event"),
+  titlePanel("Zika Travel Event (2016)"),
 
   sidebarLayout(
     sidebarPanel(
@@ -21,10 +19,12 @@ shinyUI(fluidPage(
                   label   = "Select month",
                   min     = 2,
                   max     = 6,
-                  value   = 2)),
+                  value   = 2),
+    helpText("Data: CDC zika repo (https://github.com/cdcepi/zika)")),
     
     mainPanel(
       plotOutput("stateMap")
+      
       
     )
   )
