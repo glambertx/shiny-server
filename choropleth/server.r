66a6c09b5601b991ec6135affbe4ef889661b5df
Greg Lambert <glambert2010@icloud.com>
2016-07-14 21:58:50 -0600

    Update server.r

--- choropleth/server.r
+++ choropleth/server.r
@@ -19,7 +19,7 @@ df1 <- df1[c(1,6,5,4,3,2)]
   shinyServer(function(input, output) {
     value_range <- range(df1[,2:6])
 
-    fill_scale <- scale_fill_gradientn(limits = range(value_range),colours=c("#ffffff", brewer.pal(n=9, name="YlOrRd")),na.value="#ffffff", name="Cases", drop=FALSE)
+    fill_scale <- scale_fill_gradientn(limits = range(value_range),colours=c("#ffffff", brewer.pal(n=9, name="YlOrRd")),na.value="#ffffff", name="Cases")
     output$stateMap = renderPlot({
       
       # add a progress bar
